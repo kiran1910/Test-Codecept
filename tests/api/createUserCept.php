cead086787bb1f5cb3fbc539aa828862541210c5
Nilesh Thombare <nilesh.thombare@syntrosys.com>
2016-03-28 21:22:12 +0530

    Commit 12

--- tests/api/createUserCept.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$I = new ApiTester($scenario);
-$I->wantTo('Api to create a user');
-$I->haveHttpHeader('Content-Type', 'application/json');
-$I->sendPOST('/api.php', ['user_name' => 'Mahesh1']);
-$I->seeResponseCodeIs(200);
-$I->seeResponseIsJson();
-?>
