fa25e4ee7a9a6e9e472451fe5333539c6f0a08f3
Nilesh Thombare <nilesh.thombare@syntrosys.com>
2016-03-28 14:37:46 +0530

    Second Commit

--- tests/api/createUserCept.php
+++ tests/api/createUserCept.php
@@ -2,7 +2,7 @@
 $I = new ApiTester($scenario);
 $I->wantTo('create a user');
 $I->haveHttpHeader('Content-Type', 'application/json');
-$I->sendPOST('/users', ['user_name' => 'Mahesh']);
+$I->sendPOST('/users', ['user_name' => 'Mahesh1']);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('{"message":"User Information saved successfully."}');
