d68282d65b8ed8022b07f7b2e58e65be9d64448c
Nilesh Thombare <nilesh.thombare@syntrosys.com>
2016-03-30 17:22:54 +0530

    db change

--- db_config.php
+++ db_config.php
@@ -6,6 +6,6 @@ $password = "";
 global $myConnection;
 
 $myConnection = mysqli_connect($servername, $username, $password);
-mysqli_select_db( $myConnection, "test_codecept");
+mysqli_select_db( $myConnection, "test_travis");
 
 ?>
